ba563e4b2f189c2c103b349462f27dfd63f39fc1
Iquis <iquis01@gmail.com>
2023-03-10 09:54:56 +0100

    feat: EventControl

--- GGXrdReversalTool/Controls/EventControl.xaml.cs
+++ GGXrdReversalTool/Controls/EventControl.xaml.cs
@@ -15,20 +15,16 @@ public partial class EventControl : UserControl, INotifyPropertyChanged
         InitializeComponent();
     }
 
-    // public IEnumerable<ScenarioActionTypes> ActionTypes { get; set; }
-
     public IEnumerable<ScenarioActionTypes> ActionTypes
     {
         get => (IEnumerable<ScenarioActionTypes>)GetValue(ActionTypesProperty);
         set => SetValue(ActionTypesProperty, value);
     }
-
     public static readonly DependencyProperty ActionTypesProperty = DependencyProperty.Register(
         nameof(ActionTypes), typeof(IEnumerable<ScenarioActionTypes>), typeof(EventControl), new PropertyMetadata(default(IEnumerable<ScenarioActionTypes>)));
 
-    private ScenarioActionTypes _selectedScenarioEvent;
-
-    public ScenarioActionTypes SelectedScenarioEvent
+    private ScenarioActionTypes? _selectedScenarioEvent;
+    public ScenarioActionTypes? SelectedScenarioEvent
     {
         get => _selectedScenarioEvent;
         set
@@ -43,4 +39,75 @@ public partial class EventControl : UserControl, INotifyPropertyChanged
 
     public event PropertyChangedEventHandler? PropertyChanged;
 
+
+    public int MinComboCount
+    {
+        get => (int)GetValue(MinComboCountProperty);
+        set => SetValue(MinComboCountProperty, value);
+    }
+    public static readonly DependencyProperty MinComboCountProperty = DependencyProperty.Register(
+        nameof(MinComboCount), typeof(int), typeof(EventControl), new PropertyMetadata(1));
+
+    public int MaxComboCount
+    {
+        get => (int)GetValue(MaxComboCountProperty);
+        set => SetValue(MaxComboCountProperty, value);
+    }
+    public static readonly DependencyProperty MaxComboCountProperty = DependencyProperty.Register(
+        nameof(MaxComboCount), typeof(int), typeof(EventControl), new PropertyMetadata(100));
+
+
+    public bool ShouldCheckWakingUp
+    {
+        get => (bool)GetValue(ShouldCheckWakingUpProperty);
+        set => SetValue(ShouldCheckWakingUpProperty, value);
+    }
+    public static readonly DependencyProperty ShouldCheckWakingUpProperty = DependencyProperty.Register(
+        nameof(ShouldCheckWakingUp), typeof(bool), typeof(EventControl), new PropertyMetadata(true));
+
+    public bool ShouldCheckWallSplat
+    {
+        get => (bool)GetValue(ShouldCheckWallSplatProperty);
+        set => SetValue(ShouldCheckWallSplatProperty, value);
+    }
+    public static readonly DependencyProperty ShouldCheckWallSplatProperty = DependencyProperty.Register(
+        nameof(ShouldCheckWallSplat), typeof(bool), typeof(EventControl), new PropertyMetadata(default(bool)));
+
+    public bool ShouldCheckAirTech
+    {
+        get => (bool)GetValue(ShouldCheckAirTechProperty);
+        set => SetValue(ShouldCheckAirTechProperty, value);
+    }
+    public static readonly DependencyProperty ShouldCheckAirTechProperty = DependencyProperty.Register(
+        nameof(ShouldCheckAirTech), typeof(bool), typeof(EventControl), new PropertyMetadata(default(bool)));
+
+    public bool ShouldCheckStartBlocking
+    {
+        get => (bool)GetValue(ShouldCheckStartBlockingProperty);
+        set => SetValue(ShouldCheckStartBlockingProperty, value);
+    }
+
+    public static readonly DependencyProperty ShouldCheckStartBlockingProperty = DependencyProperty.Register(
+        nameof(ShouldCheckStartBlocking), typeof(bool), typeof(EventControl), new PropertyMetadata(default(bool)));
+}
+
+public class EventControlDataTemplateSelector : DataTemplateSelector
+{
+    public DataTemplate ComboDataTemplate { get; set; } = null!;
+    public DataTemplate AnimationDataTemplate { get; set; } = null!;
+
+    public override DataTemplate SelectTemplate(object item, DependencyObject container)
+    {
+        if (item is ScenarioActionTypes actionType)
+        {
+            return actionType switch
+            {
+                ScenarioActionTypes.Animation => AnimationDataTemplate,
+                ScenarioActionTypes.Combo => ComboDataTemplate,
+                _ => new DataTemplate()
+            };
+        }
+
+        return new DataTemplate();
+    }
 }
